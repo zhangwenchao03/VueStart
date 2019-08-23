16995b4a95131d4ba0983708333f789bf4be3af2
zhangwenchao03 <1570416024@qq.com>
2019-08-23 15:34:26 +0800

    init vue pro

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastClick from 'fastclick'
+//  重置浏览器样式表
+import './assets/styles/normalize.css'
+// 解决移动端1PX边框问题
+import './assets/styles/border.css'
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+<div >
+     home
+</div>
+      <router-link to="/list">列表</router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
