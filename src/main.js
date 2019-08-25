1aa76937730322168180136e41f4dd5241018d6b
zhangwenchao03 <1570416024@qq.com>
2019-08-25 22:59:17 +0800

    looptu

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 //  重置浏览器样式表
 import 'styles/normalize.css'
 // 解决移动端1PX边框问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 /* eslint-disable no-new */
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='wraper'>
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for='item of swiperList' :key='item.id'>
+      <img class='swiper-img' :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+<script>
+export default{
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/p/tts4/1908/f8/7800d329239e1d02.jpg_640x360_3f8cce14.jpg'
+        }, {
+          id: "002",
+          imgUrl: 'http://img1.qunarzz.com/p/tts1/1908/a2/979ea68b0701a402.jpg_640x360_ba644688.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+   // >>>表示穿透。作用在wraper范围的所有类
+  .wraper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wraper
+    display: flex
+    width: 100%
+    height: 200px
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
